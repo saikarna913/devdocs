019e9c302e61443516a1fa4079562c73a66456d1
MatplotlibCircleBot <MatplotlibCircleBot@nomail>
2024-12-12 01:20:53 +0000

    Docs build of 6a84a5d408ee2fff112223e8ead94ef6ebfe9c2d

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.10.0.dev1105+g6a84a5d408',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
